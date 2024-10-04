52634468cb7902701c7febc2d0df1cac792d99b8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-03 22:46:48 -0300

    Feat: Conexão com o MySQL. Lembrador: Baixar o XAMPP para rodar o MySQL localmente

--- megafarma/src/main/java/br/com/fiap/dao/RemedioDAO.java
+++ megafarma/src/main/java/br/com/fiap/dao/RemedioDAO.java
@@ -2,19 +2,35 @@ package br.com.fiap.dao;
 
 import br.com.fiap.to.RemedioTO;
 
-import java.time.LocalDate;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class RemedioDAO {
+public class RemedioDAO extends Repository {
     public ArrayList<RemedioTO> findAll() {
         ArrayList<RemedioTO> remedios = new ArrayList<>();
-        RemedioTO remedio = new RemedioTO(1L, "Loratadina", 7.93, LocalDate.parse("2023-10-10"), LocalDate.parse
-                ("2025-10-10"));
-        remedios.add(remedio);
-        remedio = new RemedioTO(2L, "Amoxicilina", 26.50, LocalDate.now(), LocalDate.now().plusYears(2));
-        remedios.add(remedio);
-        remedio = new RemedioTO(3L, "Metformina", 9.99, LocalDate.now().minusYears(1), LocalDate.now().plusYears(1));
-        remedios.add(remedio);
+        String sql = "select * from ddd_remedios order by codigo";
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs != null) {
+                while (rs.next()) {
+                    RemedioTO remedio = new RemedioTO();
+                    remedio.setCodigo(rs.getLong("codigo"));
+                    remedio.setNome(rs.getString("nome"));
+                    remedio.setPreco(rs.getDouble("preco"));
+                    remedio.setDataDeFabricacao(rs.getDate("data_de_fabricacao").toLocalDate());
+                    remedio.setDataDeValidade(rs.getDate("data_de_validade").toLocalDate());
+                    remedios.add(remedio);
+                }
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro na consulta: " + e.getMessage());
+        } finally {
+            closeConnection();
+        }
         return remedios;
     }
 }
